218cfc95b5362aaa135dabde695f761475a72fe9
Luke Breen <l.breen1@outlook.com>
2017-04-15 10:41:57 +0100

    Migrated dictionary from csv to json and grouped together by word length

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ def unscramble_word(scrambled_word)
   dictionary_words = []
 
   # Import all the words from the dictionary file into an array
-  File.open("dictionary.csv", "r").each_line { |line| dictionary_words << line }
+  File.read("dictionary.csv").each_line { |line| dictionary_words << line }
 
   # Select words from the dictionary array which have the same letters
   answers = dictionary_words.select { |word| word.chars.sort.drop(1) == scrambled_word.chars.sort }
@@ -43,13 +43,41 @@ def find_definitions(words)
   definitions
 end
 
-# Basic terminal user interface
-puts "What is the scrambled word?"
-word = gets.chomp
-puts ""
-puts "Possible answers: "
-unscrambled_words = unscramble_word(word)
-definitions = find_definitions(unscrambled_words)
+csv_file = []
+json_file = {}
 
-# Display possible unscrambled words with their definitions
-definitions.each { |word, definition| puts "#{word} -> #{definition}"}
+File.read("dictionary.csv").each_line { |line| csv_file << line.slice(0..-2) }
+
+csv_file.each do |word|
+  json_file[word.length] = [] if json_file[word.length].nil?
+
+  json_file[word.length] << word
+end
+
+File.open("dictionary.json","w") do |f|
+  f.write(JSON.pretty_generate(json_file))
+end
+
+# # Basic terminal user interface
+# puts "What is the scrambled word?"
+# #input = gets.chomp
+# puts ""
+# puts "Possible answers: "
+# time_taken = {}
+
+# inputs = ["hello", "words", "chair", "cricket"]
+
+# inputs.each_with_index do |input, index|
+#   start_time = Time.now
+#   unscrambled_words = unscramble_word(input)
+#   definitions = find_definitions(unscrambled_words)
+
+#   # Display possible unscrambled words with their definitions
+#   definitions.each { |word, definition| puts "#{word} -> #{definition}"}
+#   end_time = Time.now
+#   time_taken[index] = {}
+#   time_taken[index][input] = end_time - start_time
+# end
+
+# puts "Total time taken:"
+# time_taken.each { |word, time| puts "#{word} -> #{time}"}
