47d96c348d44ceb3a0940828bef33fc59d090af5
Luke Breen <l.breen1@outlook.com>
2017-04-16 10:33:41 +0100

    User can add words to the dictionary

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'json'
 
 def unscramble_word(scrambled_word)
   # Import all the words from the dictionary file into an array
-  dictionary_words = JSON.parse(File.read("dictionary.json"))
+  dictionary = JSON.parse(File.read("dictionary.json"))
 
   # Select words from the dictionary array which have the same letters
-  dictionary_words[scrambled_word.length.to_s].select { |word| word.chars.sort == scrambled_word.chars.sort }
+  dictionary[scrambled_word.length.to_s].select { |word| word.chars.sort == scrambled_word.chars.sort }
 end
 
 def find_definitions(words)
@@ -16,25 +16,25 @@ def find_definitions(words)
 
   definitions = {}
 
-  # Iterate over each unscrambled word to retrieve their definition
-  words.each do |word|
-    # Construct the request url
-    url = "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{word}"
-    begin
-      # Make request to the dictionary api
-      response = RestClient.get url, {'app_id' => app_id, 'app_key' => app_key}
+  # # Iterate over each unscrambled word to retrieve their definition
+  # words.each do |word|
+  #   # Construct the request url
+  #   url = "https://od-api.oxforddictionaries.com/api/v1/entries/en/#{word}"
+  #   begin
+  #     # Make request to the dictionary api
+  #     response = RestClient.get url, {'app_id' => app_id, 'app_key' => app_key}
 
-      # Parse the api response and access the definition from the parsed_response.
-      # Simultaneously, construct the definitions hash with the unscrambled word
-      # as the key and the definition as the value
-      definitions[word.capitalize] = JSON.parse(response.body)['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0].capitalize
+  #     # Parse the api response and access the definition from the parsed_response.
+  #     # Simultaneously, construct the definitions hash with the unscrambled word
+  #     # as the key and the definition as the value
+  #     definitions[word.capitalize] = JSON.parse(response.body)['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0].capitalize
 
-      # Rescue RestClient in case of any exceptions and state 'No definition found'
-      # in the definitions hash
-      rescue RestClient::ExceptionWithResponse => e
-        definitions[word.capitalize] = "No definition found"
-    end
-  end
+  #     # Rescue RestClient in case of any exceptions and state 'No definition found'
+  #     # in the definitions hash
+  #     rescue RestClient::ExceptionWithResponse => e
+  #       definitions[word.capitalize] = "No definition found"
+  #   end
+  # end
   definitions
 end
 
@@ -53,15 +53,33 @@ def csv_to_json(csv_file_path, json_file_path)
   File.open(json_file_path,"w") { |f| f.write(JSON.pretty_generate(json_file)) }
 end
 
-# Basic terminal user interface
-puts "What is the scrambled word?"
+def add_word_to_dictionary(new_word)
+  # Retrieve the dictionary
+  # Access the array for words of the same length
+  # If the word is already in the dictionary, do not add it
+  # If the word is not in the dictionary, add it
+  # If
+  dictionary = JSON.parse(File.read("dictionary.json"))
+
+  if dictionary[new_word.length.to_s].select{ |word| word == new_word }.empty?
+    dictionary[new_word.length.to_s] << new_word
+  else
+    puts "#{new_word.capitalize} is already in the dictionary."
+  end
+end
+puts "New word"
 input = gets.chomp
-puts ""
-puts "Possible answers: "
+add_word_to_dictionary(input)
 
-unscrambled_words = unscramble_word(input)
-definitions = find_definitions(unscrambled_words)
+# # Basic terminal user interface
+# puts "What is the scrambled word?"
+# input = gets.chomp
+# puts ""
+# puts "Possible answers: "
 
-# Display possible unscrambled words with their definitions
-definitions.each { |word, definition| puts "#{word} -> #{definition}"}
+# unscrambled_words = unscramble_word(input)
+# definitions = find_definitions(unscrambled_words)
+
+# # Display possible unscrambled words with their definitions
+# definitions.each { |word, definition| puts "#{word} -> #{definition}"}
 
