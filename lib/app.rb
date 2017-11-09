34ae0e67dc9575bfa38d9617c7e07e074696229e
Luke Breen <l.breen1@outlook.com>
2017-11-09 11:54:42 +0000

    Added rubocop and fixed style issues

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,9 @@
-puts "Starting up..."
+puts 'Starting up...'
 require_relative 'dictionary'
 require_relative 'controller'
 require_relative 'router'
 
-puts "Loading dictionary..."
+puts 'Loading dictionary...'
 dictionary = Dictionary.new
 controller = Controller.new(dictionary)
 router = Router.new(controller)
